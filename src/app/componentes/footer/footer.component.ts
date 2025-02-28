51546090edbdc85dfc14c2b57302943a82776f96
ludhill <LucasDenner.hs@live.com>
2025-02-28 15:49:24 -0300

    Navgation, Footer, e Cosmeticos

--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.component.html',
   //styleUrl: './footer.component.css'
   styleUrl: './../../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
@@ -11,3 +13,21 @@ import { Component } from '@angular/core';
 export class FooterComponent {
 
 }
+/*
+import { Component,input,inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './footer.component.html',
+  //styleUrl: './footer.component.css'
+  styleUrl: '/ ../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+})
+export class FooterComponent {
+
+}
+
+*/
--- src/app/componentes/navigation/navigation.component.ts
+++ src/app/componentes/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './navigation.component.html',
   //styleUrl: './navigation.component.css'
   styleUrl: './../../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
@@ -11,3 +12,24 @@ import { Component } from '@angular/core';
 export class NavigationComponent {
 
 }
+
+/*
+
+import { Component,input,inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  //imports: [CommonModule],
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './navbar.component.html',
+  //styleUrl: './navbar.component.css'
+  styleUrl: '/ ../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+})
+export class NavbarComponent {
+
+}
+
+*/
