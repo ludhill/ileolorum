a41dacc345d11f783c88faba733f2104a231cfb9
ludhill <LucasDenner.hs@live.com>
2025-02-28 15:52:12 -0300

    Auto-generated commit

--- chunk-3HC77V2K.js
+++
@@ -1,1 +0,0 @@
-import{U as o,sa as n,ta as a,va as s,wa as p}from"./chunk-25O6TAUI.js";var u=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-about-us"]],standalone:!0,features:[p],decls:2,vars:0,template:function(e,m){e&1&&(n(0,"p"),s(1,"about-us works!"),a())}})};export{u as AboutUsComponent};
